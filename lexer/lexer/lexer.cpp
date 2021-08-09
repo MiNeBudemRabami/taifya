f26786ffbed2e9aa535e079ee2e6c1d73662c90c
MiNeBudemRabami <sho_mLyaaa@mail.ru>
2021-08-09 07:27:40 +0300

    created

---
+++ lexer/lexer/lexer.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <map>
+
+using namespace std;
+
+struct pos
+{
+	int row;
+	int numb;
+};
+
+struct lexema
+{
+	int id;
+	pos pos;
+};
+
+map <int, lexema> buffer;
+
+int main()
+{
+}
