b1ee39fd860e2010a157be6aa164679db00ae1f5
MiNeBudemRabami <smileaschelsea@gmail.com>
2021-08-13 20:55:11 +0300

    Update lexer.cpp

--- lexer/lexer/lexer.cpp
+++ lexer/lexer/lexer.cpp
@@ -13,10 +13,20 @@ struct lexema
 {
 	int id;
 	pos pos;
+	string value;
 };
 
 map <int, lexema> buffer;
 
 int main()
 {
+	string unit = "";
+	int i = 0;
+
+	for (;;)
+	{
+		cin >> unit;
+		buffer[i] = unit;
+
+	}
 }
