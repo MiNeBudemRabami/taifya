b0c1c6dc57eea2d27dafa5a16a64c21bb17f7d3f
MiNeBudemRabami <smileaschelsea@gmail.com>
2021-09-06 05:00:53 +0300

    Update lexer.cpp

--- lexer/lexer/lexer.cpp
+++ lexer/lexer/lexer.cpp
@@ -1,12 +1,29 @@
 ﻿#include <iostream>
 #include <map>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
-//vector <string> keyword = { "begin", "end" };
-//vector <char> operand = { "+", "/" };
-//vector <string> variable = { "a", "z" };
+vector <string> keyword_arr = { "begin", "end" };
+vector <char> operand_arr = { "+", "/" };
+vector <string> variable_arr = { "a", "z" };
+
+enum lexema_type
+{
+	def,
+	keyword,
+	variable,
+	constant,
+	operand
+};
+
+lexema_type get_type(string value)
+{
+	for_each(keyword_arr.cbegin(), keyword_arr.cend(),
+		{})
+	
+}
 
 struct lexema_struct
 {
@@ -14,29 +31,34 @@ struct lexema_struct
 	int row;
 	int pos;
 	string value;
+	lexema_type type;
 };
 
 int main()
 {
 	map <int, lexema_struct> bank;
 
-	lexema_struct unit = { 1, 1, 1, "" }; //id, row, pos, value
-	int id = unit.id;
+	lexema_struct lexema = { 1, 1, 1, "",  def}; //id, row, pos, value, type 
+	int id = lexema.id;
 	//char ch = '\0';
+	string unit;
 
 	for (;;)
 	{
-		cin >> unit.value;
-
-		bank[id] = unit;
-
-		id++;
-		unit.id == id;
-		unit.pos += unit.value.size() + 1;
-
-		if (unit.value == "&")
+		cin >> unit;
+		
+		if (unit == "&")
 		{
 			break;
+		}
+		else
+		{
+			lexema.value = unit;
+			get_type(unit);
+			bank[id] = lexema;
+			id++;
+			lexema.id == id;
+			lexema.pos += lexema.value.size() + 1;
 		}
 	}
 
