01fbef88eab449a2e7b3b3ce8bc93533aa58a806
MiNeBudemRabami <smileaschelsea@gmail.com>
2021-09-28 00:21:37 +0300

    ...

--- 2/SLR/SyntacticRecognizer/SyntacticRecognizer/Token.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "TokenType.h"
-struct Token
-{
-	std::string value;
-	TokenType type;
-	size_t row;
-	size_t col;
-};
--- 2/SLR/SyntacticRecognizer/SyntacticRecognizer/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "SyntaxCheck.hpp"
-#include <iostream>
-
-int main( int argc, char* argv[] ) {
-	setlocale( LC_ALL, "Russian" );
-	SyntaxCheck syntaxCheck( "Source1.txt", "Table1.txt" );
-	std::string str = syntaxCheck.Check();
-	std::cout << str;
-}
